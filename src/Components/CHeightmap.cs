e3f4613468be1e3975a193852b2bc1a4a2afc653
Tobias Ånhed <Anhedtobias@gmail.com>
2017-04-24 14:00:10 +0200

    tobfix

--- src/Components/CHeightmap.cs
+++ src/Components/CHeightmap.cs
@@ -4,7 +4,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 using Core;
-using BoundingGeometries;
 
 public class CHeightmap: Component {
     public IndexBuffer IndexBuffer { get; set; }
--- src/Components/CModel.cs
+++ src/Components/CModel.cs
@@ -8,7 +8,6 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
 using Core;
-using BoundingGeometries;
 
 /*--------------------------------------
  * CLASSES
